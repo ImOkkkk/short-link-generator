318abb4f5e47be3ac8d7d8c0a5c01818ca82625c
ImOkkkk <547717253@qq.com>
2022-05-06 16:33:18 +0800

    feat: 新增前端页面

---
+++ src/main/java/cn/imokkkk/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package cn.imokkkk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author ImOkkkk
+ * @date 2022/5/6 13:26
+ * @since 1.0
+ */
+@Configuration
+public class CorsConfig {
+
+  private CorsConfiguration buildConfig() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.addAllowedOrigin("*");
+    corsConfiguration.addAllowedHeader("*");
+    corsConfiguration.addAllowedMethod("*");
+    corsConfiguration.setAllowCredentials(true);
+    return corsConfiguration;
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", buildConfig());
+    return new CorsFilter(source);
+  }
+}
